36934e7f5860c278cc74ec2663ce094fffe9ce1c
fauvel gregory <gregory.fauvel@laplateforme.io>
2020-03-09 10:38:16 +0100

    job01 fini

--- jour01/Job01/index.php
+++ jour01/Job01/index.php
@@ -1,10 +1,14 @@
+
 <!DOCTYPE html>
 <html>
 <head>
+	<script type="text/javascript">
+var mess = alert("Hello JavaScript");
+</script>
 	<title></title>
 </head>
 <body>
 
+
 </body>
 </html>
-<script></script>

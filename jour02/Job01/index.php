f77ef051fba16a39dac55174cc13d5892302827e
fauvel gregory <gregory.fauvel@laplateforme.io>
2020-03-10 11:55:55 +0100

    jour02/Job01 fini

---
+++ jour02/Job01/index.php
@@ -0,0 +1,17 @@
+
+<html>
+<head>
+	<script type="text/javascript" src="script.js"></script>
+	<title></title>
+</head>
+
+<body>
+
+<button class="button" onclick= "citation()" >Citation</button>
+<article id="citation">La vie a beaucoup plus d’imagination que nous.</article>
+	
+</body>
+</html>
+
+
+
---
+++ jour02/Job01/script.js
@@ -0,0 +1,8 @@
+function citation() {
+var text= document.getElementById("citation").innerHTML;
+console.log(text);
+ 
+}
+
+	
+

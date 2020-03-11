a7adec52ce07fcc21d726774c4d0ac6282e80dc9
fauvel gregory <gregory.fauvel@laplateforme.io>
2020-03-11 10:45:42 +0100

    job 01 fini

---
+++ jour03/Job01/index.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html>
+<head>
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
+<script type="text/javascript" src="script.js"></script>
+</head>
+<body>
+
+<p>Le $ monétaire est un bon élément monétaire “le $ est un bon élément
+monétaire. Le # de twitter est une bonne I D. Il faudra faire le point avec la
+classe pour cacher cet élément. ”.</p>
+
+<button id="hide">Cache</button><br>
+<button id="show">Montre</button><br>
+
+</body>
+</html>
+
+
+
---
+++ jour03/Job01/script.js
@@ -0,0 +1,18 @@
+$(document).ready(function(){
+	  $("p").hide();
+	  $("#hide").hide();
+
+  $("#hide").click(function(){
+   		$("p").hide();
+ 		$("#hide").hide();
+
+  });
+   
+  $("#show").click(function(){
+    	$("p").show();
+     	$("#hide").show();
+     
+  });
+});
+
+

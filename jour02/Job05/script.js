82d5994385c55a7f6043f164134a12e8cc73f7d3
fauvel gregory <gregory.fauvel@laplateforme.io>
2020-03-11 09:22:13 +0100

    pas fini

--- jour02/Job05/script.js
+++ jour02/Job05/script.js
@@ -1,10 +1,10 @@
 window.onscroll = function() {myFunction()};
 
 function myFunction() {
-  if (document.body.scrollBottom > 300 || document.documentElement.scrollTop > 300) {
-    document.getElementById("footer").className = "foot";
+  if (document.body.scrollTop > 300 || document.documentElement.scrollTop > 300) {
+    document.getElementById("footer").className = "footer";
   } else {
-    document.getElementById("footer").className = "";
+    document.getElementById("footer").className = "foot";
   }
 }
 

46c200a0aca8f405a0f5627cc94c38d16c4aa9a8
fauvel gregory <gregory.fauvel@laplateforme.io>
2020-03-12 14:47:26 +0100

    job 02 FINI

---
+++ jour04/Job02/index.php
@@ -0,0 +1,23 @@
+
+<html>
+<head>
+<link rel="stylesheet" type="text/css" href="style.css">
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
+<script type="text/javascript" src="script.js"></script>
+</head>
+
+
+<body>
+
+
+<button id="button" onclick="jsonValueskey()">Envoie</button>
+<p id="demo"></p>
+
+
+
+</body>
+</html>
+
+
+
+
---
+++ jour04/Job02/script.js
@@ -0,0 +1,13 @@
+
+function jsonValueskey(test,test){
+
+var txt = '{"name":"La Plateforme_", "address":"8 rue d hozier", "city":"Marseille", "creation":"2019"}';
+var obj = JSON.parse(txt);
+console.log(obj.city);
+             
+             };
+         
+
+
+
+
